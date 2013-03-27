aa91ec46f8a541d9a9a73d2a819df7a7b8db1dad
josemaria <josemaria@m9d515x>
2013-03-27 14:06:32 +0100

    minor change

--- vegecom/vegecom-rest/src/main/java/com/veisite/vegecom/rest/error/RestExceptionHandler.java
+++ vegecom/vegecom-rest/src/main/java/com/veisite/vegecom/rest/error/RestExceptionHandler.java
@@ -3,22 +3,17 @@ package com.veisite.vegecom.rest.error;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 
 import com.veisite.vegecom.rest.SerializationService;
 
-public class RestExceptionHandler extends AbstractHandlerExceptionResolver implements InitializingBean {
+public class RestExceptionHandler extends AbstractHandlerExceptionResolver {
 
+	@Autowired
     private SerializationService serializationService = null;
 	
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	protected ModelAndView doResolveException(HttpServletRequest request,
 			HttpServletResponse response, Object handler, Exception ex) {
